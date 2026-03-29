9a1de1bd54ddf34c1b8fcf27fd87c00f3e85164f
Daniel <danicoder2001@gmail.com>
2026-03-29 16:22:19 -0500

    limpieza total de handlers en servidor

--- src/components/layout/SafeDashboardLayout.tsx
+++ src/components/layout/SafeDashboardLayout.tsx
@@ -27,7 +27,7 @@ export default function SafeDashboardLayout({ children, }: { children: React.Rea
             <div className="flex flex-col min-h-screen transition-all duration-300 lg:pl-64 w-full">
 
                 {/* Header receives the toggle function */}
-                <Header onMenuClick={() => setIsSidebarOpen(true)} />
+                <Header />
                 {/* PAGE CONTENT: The 'children' inject here */}
                 <main className="flex-1 p-4 md:p-8 overflow-y-auto">
                     {children}
